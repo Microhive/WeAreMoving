476becaa98e1a4dccf8a7581c161728e0d0f0b97
Eiler Ka-Nid Yodla-Ong Poulsen <eiler@Eilers-MacBook-Pro.local>
2022-01-29 22:51:16 +0000

    Adding music and menu

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
@@ -14,5 +15,13 @@ public class Menu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("Level", LoadSceneMode.Additive);
+    }
+
+    public void EndGame() {
+        Application.Quit();
     }
 }
